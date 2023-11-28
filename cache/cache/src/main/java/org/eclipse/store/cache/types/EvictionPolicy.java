892e6fd2e60d2349ba6bd5a45d3ee5b9568dda1a
hg-ms <53219833+hg-ms@users.noreply.github.com>
2023-11-28 14:13:48 +0100

    generated license text

--- cache/cache/src/main/java/org/eclipse/store/cache/types/EvictionPolicy.java
+++ cache/cache/src/main/java/org/eclipse/store/cache/types/EvictionPolicy.java
@@ -1,6 +1,20 @@
 
 package org.eclipse.store.cache.types;
 
+/*-
+ * #%L
+ * MicroStream Cache
+ * %%
+ * Copyright (C) 2023 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+
 import static org.eclipse.serializer.util.X.notNull;
 
 import java.util.Comparator;
