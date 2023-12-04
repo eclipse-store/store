d214bc921265533e1319ca0be882288777fae992
hg-ms <53219833+hg-ms@users.noreply.github.com>
2023-12-04 06:57:04 +0100

    license text

--- storage/storage/src/main/java/org/eclipse/store/storage/types/StorageRequestTaskTransactionsLogCleanup.java
+++ storage/storage/src/main/java/org/eclipse/store/storage/types/StorageRequestTaskTransactionsLogCleanup.java
@@ -1,5 +1,19 @@
 package org.eclipse.store.storage.types;
 
+/*-
+ * #%L
+ * EclipseStore Storage
+ * %%
+ * Copyright (C) 2023 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+
 public interface StorageRequestTaskTransactionsLogCleanup extends StorageRequestTask
 {
 	public boolean result();
