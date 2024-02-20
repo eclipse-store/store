c4385633cdc88b3da8bbd01af72569dc4c09fc9c
Zdenek Jonas <z.jonas@microstream.one>
2024-02-20 13:41:30 +0100

    remove file from not corresponding part

--- examples/spring-boot3-simple/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module spring.boot3.simple {
-    requires org.eclipse.serializer.persistence;
-    requires spring.boot.autoconfigure;
-    requires spring.context;
-    requires spring.web;
-    requires org.eclipse.store.integrations.spring.boot;
-    requires org.eclipse.store.storage.embedded;
-}
