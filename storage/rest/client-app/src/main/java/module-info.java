f7a99eadab1346e32a49f36ee39e923483275f47
hg-ms <53219833+hg-ms@users.noreply.github.com>
2024-02-21 10:39:02 +0100

    fixed build errors in eclipse IDE

--- storage/rest/client-app/src/main/java/module-info.java
+++ storage/rest/client-app/src/main/java/module-info.java
@@ -2,7 +2,7 @@
  * #%L
  * EclipseStore Storage REST Client App
  * %%
- * Copyright (C) 2023 MicroStream Software
+ * Copyright (C) 2023 - 2024 MicroStream Software
  * %%
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -11,41 +11,37 @@
  * SPDX-License-Identifier: EPL-2.0
  * #L%
  */
-
-module org.eclipse.store.storage.restclient.app
-{
+module org.eclipse.store.storage.restclient.app {
 	exports org.eclipse.store.storage.restclient.app.types;
 	exports org.eclipse.store.storage.restclient.app.ui;
-	
-	provides com.vaadin.flow.server.VaadinServiceInitListener
-	    with org.eclipse.store.storage.restclient.app.types.ApplicationServiceInitListener
-	;
-	
-	requires transitive flow.data;
-	requires transitive flow.html.components;
-	requires transitive flow.server;
-	requires transitive gwt.elemental;
-	requires transitive org.eclipse.serializer.base;
-	requires transitive org.eclipse.store.storage.restadapter;
-	requires transitive org.eclipse.store.storage.restclient;
-	requires transitive org.eclipse.store.storage.restclient.jersey;
-	requires transitive org.apache.tomcat.embed.core;
-	requires transitive org.slf4j;
-	requires transitive spring.beans;
-	requires transitive spring.boot;
-	requires transitive spring.boot.autoconfigure;
-	requires transitive spring.context;
-	requires transitive spring.core;
-	requires transitive spring.web;
-	requires transitive vaadin.spring;
-	requires transitive vaadin.button.flow;
-	requires transitive vaadin.combo.box.flow;
-	requires transitive vaadin.details.flow;
-	requires transitive vaadin.grid.flow;
-	requires transitive vaadin.lumo.theme;
-	requires transitive vaadin.notification.flow;
-	requires transitive vaadin.ordered.layout.flow;
-	requires transitive vaadin.split.layout.flow;
-	requires transitive vaadin.tabs.flow;
-	requires transitive vaadin.text.field.flow;
+
+	requires flow.data;
+	requires flow.html.components;
+	requires flow.server;
+	requires gwt.elemental;
+	requires org.apache.tomcat.embed.core;
+	requires org.eclipse.serializer.base;
+	requires org.eclipse.store.storage.restadapter;
+	requires org.eclipse.store.storage.restclient;
+	requires org.eclipse.store.storage.restclient.jersey;
+	requires org.slf4j;
+	requires spring.beans;
+	requires spring.boot;
+	requires spring.boot.autoconfigure;
+	requires spring.context;
+	requires spring.core;
+	requires spring.web;
+	requires vaadin.button.flow;
+	requires vaadin.combo.box.flow;
+	requires vaadin.details.flow;
+	requires vaadin.flow.components.base;
+	requires vaadin.grid.flow;
+	requires vaadin.lumo.theme;
+	requires vaadin.notification.flow;
+	requires vaadin.ordered.layout.flow;
+	requires vaadin.renderer.flow;
+	requires vaadin.split.layout.flow;
+	requires vaadin.spring;
+	requires vaadin.tabs.flow;
+	requires vaadin.text.field.flow;
 }
