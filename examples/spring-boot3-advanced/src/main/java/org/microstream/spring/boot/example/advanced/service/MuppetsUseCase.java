1c4ed48997019c1339895046a4f681dd9d8cdad2
Zdenek Jonas <z.jonas@microstream.one>
2024-02-22 08:08:46 +0100

    locally working build with module system

--- examples/spring-boot3-advanced/src/main/java/org/microstream/spring/boot/example/advanced/service/MuppetsUseCase.java
+++ examples/spring-boot3-advanced/src/main/java/org/microstream/spring/boot/example/advanced/service/MuppetsUseCase.java
@@ -1,5 +1,19 @@
 package org.microstream.spring.boot.example.advanced.service;
 
+/*-
+ * #%L
+ * EclipseStore Example Spring Boot3 Advanced
+ * %%
+ * Copyright (C) 2023 - 2024 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+
 import org.microstream.spring.boot.example.advanced.storage.MuppetStorage;
 import org.springframework.stereotype.Component;
 
