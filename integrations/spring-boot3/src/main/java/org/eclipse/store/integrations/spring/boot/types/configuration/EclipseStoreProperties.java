bf148442e93d6110f0dc6e9e7b81a9f7c38a36ab
Zdenek Jonas <z.jonas@microstream.one>
2024-03-19 14:11:18 +0100

    rel ver 1.3.0

--- integrations/spring-boot3/src/main/java/org/eclipse/store/integrations/spring/boot/types/configuration/EclipseStoreProperties.java
+++ integrations/spring-boot3/src/main/java/org/eclipse/store/integrations/spring/boot/types/configuration/EclipseStoreProperties.java
@@ -1,5 +1,19 @@
 package org.eclipse.store.integrations.spring.boot.types.configuration;
 
+/*-
+ * #%L
+ * integrations-spring-boot3
+ * %%
+ * Copyright (C) 2023 - 2024 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.context.annotation.Configuration;
