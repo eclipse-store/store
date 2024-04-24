e818ffc51c04cf203486fe53e86ba2a8b0ad20f4
Zdenek Jonas <z.jonas@microstream.one>
2024-04-24 11:39:22 +0200

    fixes after review
    (cherry picked from commit bd937d2f1712171e0318a1c7ed95ae56fa8cfd86)

--- integrations/spring-boot3/src/test/java/test/eclipse/store/integrations/spring/boot/DisableStorageAutoCreateTest.java
+++ integrations/spring-boot3/src/test/java/test/eclipse/store/integrations/spring/boot/DisableStorageAutoCreateTest.java
@@ -1,5 +1,19 @@
 package test.eclipse.store.integrations.spring.boot;
 
+/*-
+ * #%L
+ * EclipseStore Integrations SpringBoot
+ * %%
+ * Copyright (C) 2023 - 2024 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+
 import org.eclipse.store.storage.embedded.types.EmbeddedStorageManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
