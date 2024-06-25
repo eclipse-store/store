5e606da998fe24ab2c2de843041e7335508da7ca
Zdenek Jonas <z.jonas@microstream.one>
2024-06-25 09:58:05 +0200

    fix licence header

--- examples/extension-wrapper/src/main/java/org/eclipse/store/examples/extensionwrapper/PersistenceStorerWrapper.java
+++ examples/extension-wrapper/src/main/java/org/eclipse/store/examples/extensionwrapper/PersistenceStorerWrapper.java
@@ -1,21 +1,20 @@
 package org.eclipse.store.examples.extensionwrapper;
 
-import org.eclipse.serializer.persistence.types.PersistenceCommitListener;
-
 /*-
  * #%L
  * EclipseStore Example Extension Wrapper
  * %%
- * Copyright (C) 2023 MicroStream Software
+ * Copyright (C) 2023 - 2024 MicroStream Software
  * %%
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  * #L%
  */
 
+import org.eclipse.serializer.persistence.types.PersistenceCommitListener;
 import org.eclipse.serializer.persistence.types.PersistenceStorer;
 
 /**
