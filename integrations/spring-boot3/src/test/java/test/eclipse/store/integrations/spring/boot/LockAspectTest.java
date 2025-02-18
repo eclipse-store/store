fe2665872f0b9ac663a96be93dc9adab6a7ba898
Zdenek Jonas <z.jonas@microstream.one>
2025-02-18 09:28:59 +0100

    refactor: update Maven build commands to use 'clean' before 'package' and 'deploy'

--- integrations/spring-boot3/src/test/java/test/eclipse/store/integrations/spring/boot/LockAspectTest.java
+++ integrations/spring-boot3/src/test/java/test/eclipse/store/integrations/spring/boot/LockAspectTest.java
@@ -1,5 +1,19 @@
 package test.eclipse.store.integrations.spring.boot;
 
+/*-
+ * #%L
+ * EclipseStore Integrations SpringBoot
+ * %%
+ * Copyright (C) 2023 - 2025 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+
 import org.eclipse.store.integrations.spring.boot.types.concurrent.LockAspect;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
