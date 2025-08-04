a7cb95f5d50f593efc86d67001d1aad3db2573a7
Zdenek Jonas <z.jonas@microstream.one>
2025-08-04 16:07:01 +0200

    test: enhance MultipleValueIndexTest with storage validation and entity updates

--- gigamap/gigamap/src/test/java/org/eclipse/store/gigamap/indexer/MultipleValueIndexTest.java
+++ gigamap/gigamap/src/test/java/org/eclipse/store/gigamap/indexer/MultipleValueIndexTest.java
@@ -9,28 +9,30 @@ package org.eclipse.store.gigamap.indexer;
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  * #L%
  */
 
-import org.eclipse.store.gigamap.types.GigaMap;
-import org.eclipse.store.gigamap.types.GigaQuery;
-import org.eclipse.store.gigamap.types.IndexerMultiValue;
-import org.eclipse.store.gigamap.types.IndexerString;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.eclipse.store.gigamap.types.*;
+import org.eclipse.store.storage.embedded.types.EmbeddedStorage;
+import org.eclipse.store.storage.embedded.types.EmbeddedStorageManager;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class MultipleValueIndexTest
 {
 
+    @TempDir
+    Path tempDir;
 
     @Test
     void indexEntity_UnsupportedOperationException()
@@ -85,8 +87,24 @@ public class MultipleValueIndexTest
         gigaMap.update(patient1, p -> {
              p.setIdentifiers(List.of("123", "abc"));
         });
-        
+
         assertEquals(1L, gigaMap.query(patientIdentifierIndexer.is("abc")).count());
+
+        try (EmbeddedStorageManager storage = EmbeddedStorage.start(gigaMap, tempDir))
+        {
+        }
+
+        GigaMap<Patient> loadedGigaMap = GigaMap.New();
+        try (EmbeddedStorageManager storage = EmbeddedStorage.start(loadedGigaMap, tempDir)) {
+            Patient patient = loadedGigaMap.get(loadedGigaMap.highestUsedId());
+            BitmapIndex<Patient, String> loadedIndex = loadedGigaMap.index().bitmap().get("org.eclipse.store.gigamap.indexer.MultipleValueIndexTest.PatientIdentifierIndexer");
+            assertNotNull(loadedIndex);
+            loadedGigaMap.update(patient, p -> {
+                p.setIdentifiers(List.of("123", "xxx"));
+            });
+            loadedGigaMap.store();
+            assertEquals(1L, loadedGigaMap.query(loadedIndex.is("xxx")).count());
+        }
     }
 
     @Test
@@ -100,8 +118,24 @@ public class MultipleValueIndexTest
         gigaMap.addAll(patient1, patient2);
 
         gigaMap.remove(patient1);
-        
+
         assertEquals(1L, gigaMap.size());
+
+        try (EmbeddedStorageManager storage = EmbeddedStorage.start(gigaMap, tempDir))
+        {
+        }
+
+        GigaMap<Patient> loadedGigaMap = GigaMap.New();
+        try (EmbeddedStorageManager storage = EmbeddedStorage.start(loadedGigaMap, tempDir)) {
+            Patient patient = loadedGigaMap.get(loadedGigaMap.highestUsedId());
+            loadedGigaMap.remove(patient);
+            loadedGigaMap.store();
+            assertTrue(loadedGigaMap.isEmpty());
+
+            Patient patient3 = new Patient("Jack", 35, List.of("123", "456"));
+            loadedGigaMap.add(patient3);
+            loadedGigaMap.store();
+        }
     }
 
     private static class PatientIdentifierIndexer extends IndexerMultiValue.Abstract<Patient, String> {
@@ -145,12 +179,12 @@ public class MultipleValueIndexTest
         {
             return identifiers;
         }
-        
+
         public void setIdentifiers(final List<String> identifiers)
         {
             this.identifiers = identifiers;
         }
-        
+
         public void setName(String name)
         {
             this.name = name;
