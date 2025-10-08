648036eee4f16a44712efddc45005e840ecf6e93
Zdenek Jonas <z.jonas@microstream.one>
2025-10-08 15:16:40 +0200

    chore: add license headers to Comparable test files

--- gigamap/gigamap/src/test/java/org/eclipse/store/gigamap/indexer/comparable/CustomComparableTest.java
+++ gigamap/gigamap/src/test/java/org/eclipse/store/gigamap/indexer/comparable/CustomComparableTest.java
@@ -1,5 +1,19 @@
 package org.eclipse.store.gigamap.indexer.comparable;
 
+/*-
+ * #%L
+ * EclipseStore GigaMap
+ * %%
+ * Copyright (C) 2023 - 2025 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.nio.file.Path;
