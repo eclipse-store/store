bf4270ae585652488c5eeb97494169f43311bdb3
fh-ms <f.habermann@microstream.one>
2026-02-02 15:59:01 +0100

    Add license header

--- gigamap/jvector/src/main/java/org/eclipse/store/gigamap/jvector/VectorEntry.java
+++ gigamap/jvector/src/main/java/org/eclipse/store/gigamap/jvector/VectorEntry.java
@@ -1,5 +1,19 @@
 package org.eclipse.store.gigamap.jvector;
 
+/*-
+ * #%L
+ * EclipseStore GigaMap JVector
+ * %%
+ * Copyright (C) 2023 - 2026 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+
 import org.eclipse.store.gigamap.types.BinaryIndexerLong;
 import org.eclipse.store.gigamap.types.Condition;
 
