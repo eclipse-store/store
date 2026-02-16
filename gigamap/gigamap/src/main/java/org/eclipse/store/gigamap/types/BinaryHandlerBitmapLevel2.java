f798208040b46423c5a50787d5fb55ffc20544b4
hg-ms <53219833+hg-ms@users.noreply.github.com>
2026-02-16 14:37:30 +0100

    Supporting gigmap handler initialization for LegacyTypeHandler

--- gigamap/gigamap/src/main/java/org/eclipse/store/gigamap/types/BinaryHandlerBitmapLevel2.java
+++ gigamap/gigamap/src/main/java/org/eclipse/store/gigamap/types/BinaryHandlerBitmapLevel2.java
@@ -17,6 +17,8 @@ package org.eclipse.store.gigamap.types;
 import org.eclipse.serializer.memory.XMemory;
 import org.eclipse.serializer.persistence.binary.types.AbstractBinaryHandlerCustom;
 import org.eclipse.serializer.persistence.binary.types.Binary;
+import org.eclipse.serializer.persistence.binary.types.BinaryTypeHandlerSupplier;
+import org.eclipse.serializer.persistence.types.PersistenceLegacyTypeHandler;
 import org.eclipse.serializer.persistence.types.PersistenceLoadHandler;
 import org.eclipse.serializer.persistence.types.PersistenceReferenceLoader;
 import org.eclipse.serializer.persistence.types.PersistenceStoreHandler;
@@ -29,7 +31,7 @@ import org.eclipse.serializer.typing.XTypes;
  * It extends {@link AbstractBinaryHandlerCustom} to support binary-based I/O operations
  * specific to {@link BitmapLevel2}.
  */
-public class BinaryHandlerBitmapLevel2 extends AbstractBinaryHandlerCustom<BitmapLevel2>
+public class BinaryHandlerBitmapLevel2 extends AbstractBinaryHandlerCustom<BitmapLevel2> implements BinaryTypeHandlerSupplier<BitmapLevel2> 
 {
 	public static BinaryHandlerBitmapLevel2 New()
 	{
@@ -111,6 +113,12 @@ public class BinaryHandlerBitmapLevel2 extends AbstractBinaryHandlerCustom<Bitma
 	public void iterateLoadableReferences(final Binary data, final PersistenceReferenceLoader iterator)
 	{
 		// no-op
+	}
+
+	@Override
+	public PersistenceLegacyTypeHandler<Binary, BitmapLevel2> getLegacyTypeHandler()
+	{
+		return BinaryLegacyTypeHandlerBitmapLevel2.New();
 	}
 	
 }
