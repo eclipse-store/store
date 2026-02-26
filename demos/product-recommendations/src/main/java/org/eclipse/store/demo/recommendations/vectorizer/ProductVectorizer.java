06b0164ce57952dc38063695eb2f1325ededb1d2
fh-ms <f.habermann@microstream.one>
2026-02-26 12:55:19 +0100

    Enhance product vectorization by adding support for batch vectorization and refactoring text generation logic.

--- demos/product-recommendations/src/main/java/org/eclipse/store/demo/recommendations/vectorizer/ProductVectorizer.java
+++ demos/product-recommendations/src/main/java/org/eclipse/store/demo/recommendations/vectorizer/ProductVectorizer.java
@@ -1,8 +1,12 @@
 package org.eclipse.store.demo.recommendations.vectorizer;
 
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import org.eclipse.store.demo.recommendations.model.Product;
 import org.eclipse.store.gigamap.jvector.Vectorizer;
+
+import java.util.List;
 
 public class ProductVectorizer extends Vectorizer<Product>
 {
@@ -16,6 +20,23 @@ public class ProductVectorizer extends Vectorizer<Product>
 	@Override
 	public float[] vectorize(final Product product)
 	{
-		return this.embeddingModel.embed(product.name() + " " + product.category()).content().vector();
+		return this.embeddingModel.embed(toText(product)).content().vector();
+	}
+
+	@Override
+	public List<float[]> vectorizeAll(final List<? extends Product> entities)
+	{
+		return this.embeddingModel.embedAll(
+				entities.stream().map(ProductVectorizer::toText).map(TextSegment::from).toList()
+			)
+			.content()
+			.stream()
+			.map(Embedding::vector)
+			.toList();
+	}
+
+	private static String toText(final Product product)
+	{
+		return product.name() + " " + product.category();
 	}
 }
