09a5ae22e94eac20e4b208ea60b1fcf46ad58d94
fh-ms <f.habermann@microstream.one>
2026-03-31 15:48:55 +0200

    Add Eclipse Public License 2.0 to demos and include copyright headers in source files

--- demos/country-explorer/src/main/java/org/eclipse/store/demo/countries/index/LocationIndex.java
+++ demos/country-explorer/src/main/java/org/eclipse/store/demo/countries/index/LocationIndex.java
@@ -1,5 +1,19 @@
 package org.eclipse.store.demo.countries.index;
 
+/*-
+ * #%L
+ * EclipseStore Demo Country Explorer
+ * %%
+ * Copyright (C) 2023 - 2026 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+
 import org.eclipse.store.demo.countries.model.Country;
 import org.eclipse.store.gigamap.types.SpatialIndexer;
 
